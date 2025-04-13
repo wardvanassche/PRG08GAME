61952e9ecc7f67cb6d31cef1f61b951565d9dfce
Ward <1060834@hr.nl>
2025-04-13 21:08:47 +0200

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /** @type {import('vite').UserConfig} */
 
 export default defineConfig({
-    base: "./",
+    base: "",
 
     build: {
         outDir: 'docs',
